ba9cf8956e89bd1395aa2519c9eaf232b44f4309
Aaron Wood <aaronjwood@gmail.com>
2011-08-16 00:14:15 -0400

    Initial commit (jumpstarted)

---
+++ CPU/Cpu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Diagnostics;
+
+namespace Sharp_Profiler.CPU
+{
+    class Cpu
+    {
+        private PerformanceCounter[] performance;
+
+        public Cpu()
+        {
+            performance = new PerformanceCounter[countCores()];
+        }
+
+        //Return all performance counters
+        public PerformanceCounter[] getCounters()
+        {
+            return performance;
+        }
+
+        //Return number of cores (with hyperthreading)
+        public int countCores()
+        {
+            return Environment.ProcessorCount;
+        }
+
+        //Return number of cores (without hyperthreading)
+        public int countCpu()
+        {
+            int coreCount = 0;
+            foreach (var item in new ManagementObjectSearcher("SELECT NumberOfCores FROM Win32_Processor").Get())
+            {
+                coreCount += int.Parse(item["NumberOfCores"].ToString());
+            }
+            return coreCount;
+        }
+    }
+}
---
+++ SharpProfiler.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Threading;
+using Sharp_Profiler.CPU;
+
+namespace Sharp_Profiler
+{
+    public partial class SharpProfiler : Window
+    {
+        private DispatcherTimer timer;
+
+        public SharpProfiler()
+        {
+            InitializeComponent();
+            Cpu cpu = new Cpu();
+            timer = new DispatcherTimer();
+
+            cpuInfo.Text = "Number of logical processors: " + cpu.countCores().ToString()+Environment.NewLine;
+            cpuInfo.Text += "Number of physical cores: " + cpu.countCpu();
+            //Build the performance counters
+            for (int i = 0; i < cpu.countCores(); i++)
+            {
+                cpu.getCounters()[i] = new PerformanceCounter("Processor", "% Processor Time", i.ToString());
+                cpuList.Items.Add("Core #" + i + ": " + cpu.getCounters()[i].NextValue().ToString("0.##") + "%");
+
+            }
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += delegate(object sender, EventArgs e)
+            {
+                for (int i = 0; i < cpuList.Items.Count; i++)
+                {
+                    cpuList.Items[i] = "Core #" + i + ": " + cpu.getCounters()[i].NextValue().ToString("0.##") + "%";
+                }
+            };
+            timer.Start();
+        }
+    }
+}
