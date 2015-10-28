0d16e1f7a5160d5f78a87b791bd79a3df0cd67cb
Aaron Wood <aaronjwood@gmail.com>
2015-10-28 00:34:29 -0400

    Added functionality to get the total capacity based on all of the installed sticks of RAM

--- Memory/Ram.cs
+++ Memory/Ram.cs
@@ -1,5 +1,6 @@
 ﻿using Sharp_Profiler.Hardware;
 using System;
+using System.Collections.Generic;
 
 namespace Sharp_Profiler.Memory
 {
@@ -14,6 +15,29 @@ namespace Sharp_Profiler.Memory
             try
             {
                 return (string)this.queryWmi("SELECT BankLabel FROM Win32_PhysicalMemory", "BankLabel");
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the total capacity of the system's physical memory
+        /// </summary>
+        /// <returns>Total capacity of the physical memory</returns>
+        public UInt64? getCapacity()
+        {
+            try
+            {
+                UInt64 size = 0;
+                List<object> capacities = this.queryWmi("SELECT Capacity FROM Win32_PhysicalMemory", "Capacity", 0);
+                foreach (UInt64 item in capacities)
+                {
+                    size += item;
+                }
+                size = size / 1024 / 1024;
+                return size;
             }
             catch (Exception e)
             {
--- Windows/SharpProfiler.xaml.cs
+++ Windows/SharpProfiler.xaml.cs
@@ -111,6 +111,7 @@ namespace Sharp_Profiler
         {
             Ram memory = new Ram();
             memoryBankLabel.Content = memory.getBankLabel() ?? "Unknown";
+            memoryCapacity.Content = (memory.getCapacity()?.ToString() ?? "Unknown") + " MB";
         }
     }
 }
